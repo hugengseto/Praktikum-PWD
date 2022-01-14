20e2612afc7544cdcd6b288f8c6a598fd5e65b19
hugengsetopranowo <hugengseto@gmail.com>
2022-01-14 13:08:23 +0700

    struktur organisasi

---
+++ RESPONSI (Aplikasi Layanan OSIS-ISMA)/struktur.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <link rel="stylesheet" href="style.css">
+  <style>
+    h2{
+      margin : 40px;
+      padding: 10px;
+      font-family: verdana;
+      color: #fff;
+      border: 1px solid yellow;
+      border-radius: 10px;
+      background: #026E05;
+      text-align: center;
+    }
+    .footer{
+      margin-top:55%;
+    }
+    .footer h2{
+      margin: 20px;
+      padding: 0px;
+      border: none;
+    }
+
+    .putra{
+      float : left;
+    }
+
+    .putri {
+      float : right;
+    }
+    .putra img, .putri img{
+      width: 500px;
+      border: 8px solid white;
+    }
+  </style>
+ 
+  <title>Struktur ISMA</title>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <div class="judul">
+        <img src="img/logo2.jpg" alt="">
+     </div>
+
+    <nav>
+      <a href="index.html">Home</a>
+      <a href="#">Struktur</a>
+      <a href="berita.php">Berita</a>
+      <a href="pertokoanview.php">Pertokoan</a>
+      <a href="galeri.php">Galeri</a>
+      <div class="animation start-home"></div>
+    </nav>
+    </header>
+    <p>IKATAN SANTRI MA'HAD AS-SALAM</p>
+    <hr>
+    
+    <div class="putra">
+        <h2>Pengurus Putra</h2>
+        <img src="img/putra.jpg" alt="">
+    </div>
+    <div class="putri">
+        <h2>Pengurus Putri</h2>
+        <img src="img/putri.jpg" alt="">
+    </div>
+    
+      
+    <div class="footer">
+      <div class="media">
+        <h2>Social media</h2>
+        <a href="https://instagram.com/lpi.assalam_official?igshid=1rbldw0yagt3s" target="_blank"><img src="img/instagram.png" alt="instagram" ></a>
+        <a href="https://web.facebook.com/lpi.assalam.official" target="_blank"><img src="img/facebook.png" alt="facebook"></a>
+        <a href="https://youtube.com/c/LpiAssalamChannel" target="_blank"><img src="img/youtube.png" alt="youtube"></a>
+      </div>
+    <hr>
+    <p>Copyright © 2021 by  <a href="https://wa.me/6282299049310" target="_blank">HUGENG SETO PRANOWO</a> </p>
+    </div>
+ </div>
+</body>
+</html>
